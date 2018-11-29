d9a0b1c3f24b7b8bae97a008c74eb6963d97043d
WuGuiXin <EyckWu@gmail.com>
2018-11-29 19:02:42 +0800

    Initial commit

---
+++ ch00/HelloTensorFlow.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# __author__ = eyckwu
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
+
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print(sess.run(hello))
+
+a = tf.constant(10)
+b = tf.constant(32)
+print(sess.run(a+b))
